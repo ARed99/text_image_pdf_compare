4cffb442a33c458ebb97ee605f9716f11396e1f5
ajinvarghesechandy <chandyajin@gmail.com>
2023-10-19 13:03:49 +0530

    added style to .box

---
+++ src/components/TextCompare.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { diffArrays } from 'diff'
+
+    let text1 = ''
+    let text2 = ''
+    let difference = []
+    
+    function compareText(){
+
+        if(text1 != '' || text2 != ''){
+            const diff = diffArrays(text1.split(" ") , text2.split(" "))
+            console.log(diff);
+        }
+
+        if(text1 == '' || text2 == ''){
+            alert("no text found")
+        }
+  
+    }
+</script>
+<div class="text-compare">
+    <h1>
+        Compare text
+    </h1>
+<div class="inputs">
+    <textarea bind:value={text1} name="text1" id="text1" cols="30" rows="10"></textarea>
+    <textarea bind:value={text2} name="text2" id="text2" cols="30" rows="10"></textarea>
+    
+</div>
+<div class="button">
+    <button on:click={compareText} >Find difference</button>
+</div>    
+</div>
+
+<style>
+    
+.inputs{
+width: 300px;
+height: 400px;
+
+}
+</style>

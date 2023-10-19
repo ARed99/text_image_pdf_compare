e2cad73581e265dc399fa86a654480fe37122283
ajinvarghesechandy <chandyajin@gmail.com>
2023-10-19 13:03:27 +0530

    added TextCompare componet

---
+++ src/routes/compare-text/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import TextCompare from "../../components/TextCompare.svelte";
+
+</script>
+<div class="box">
+    
+    <TextCompare/>
+</div>
+
+<style>
+    .box{
+        display: grid;
+        align-items: center;
+        place-items: center;
+        width: 100%;
+        height: 100vh;
+    }
+</style>

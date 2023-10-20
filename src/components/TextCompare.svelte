8abf11bb40a6483d6041770fe248438d654c9832
ajinvarghesechandy <chandyajin@gmail.com>
2023-10-20 10:05:59 +0530

    some new styles

--- src/components/TextCompare.svelte
+++ src/components/TextCompare.svelte
@@ -23,7 +23,7 @@
         Compare text
     </h1>
 <div class="inputs">
-    <textarea bind:value={text1} name="text1" id="text1" cols="30" rows="10"></textarea>
+    <textarea bind:value={text1} name="text1" id="text1" cols="30" rows="10" ></textarea>
     <textarea bind:value={text2} name="text2" id="text2" cols="30" rows="10"></textarea>
     
 </div>
@@ -35,10 +35,16 @@
 <style>
     
 .inputs{
-width: 800px;
+width: 1000px;
 height: 500px;
 background-color: blue;
 display:flex;
-
+justify-content: space-evenly;
+}
+textarea{
+    font-size: large;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    resize: none;
+    width: 100%;
 }
 </style>

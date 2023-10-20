f6e0ce9429d348c4810f7c16c44543df2674d596
ajinvarghesechandy <chandyajin@gmail.com>
2023-10-20 10:16:11 +0530

    added function for displaying diff

--- src/components/TextCompare.svelte
+++ src/components/TextCompare.svelte
@@ -3,24 +3,28 @@
 
 	let text1 = '';
 	let text2 = '';
-	let difference = [];
+	
 
 	function compareText() {
 		if (text1 != '' || text2 != '') {
-			const diff = diffArrays(text1.split(' '), text2.split(' '));
-			console.log(diff);
+			displayDifference(diffArrays(text1.split(' '), text2.split(' ')));
+			
 		}
 
 		if (text1 == '' || text2 == '') {
 			alert('no text found');
 		}
 	}
+
+    function displayDifference(diff){
+         console.log(diff)
+    }
 </script>
 
 <div class="text-compare">
 	<h1>Compare text</h1>
 	<div class="inputs">
-		<textarea bind:value={text1} name="text1" id="text1" cols="30" rows="10" />
+		<textarea  bind:value={text1} name="text1" id="text1" cols="30" rows="10" />
 		<textarea bind:value={text2} name="text2" id="text2" cols="30" rows="10" />
 	</div>
 	<div class="button">

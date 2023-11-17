e3d6072a62e89e75095ba9262deda9d70c9c14d7
ajinvarghesechandy1 <chandyajin@gmail.com>
2023-11-17 22:21:43 +0530

    basic navbar

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+      <li><strong>Compare</strong></li>
+    </ul>
+    <ul>
+      <li><a href="/compare-pdf">Pdf</a></li>
+      <li><a href="/compare-text">Text</a></li>
+      <li><a href="/compare-image">Image</a></li>
+    </ul>
+  </nav>

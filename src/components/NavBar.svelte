5721212cef0b1859d0a4687f9eb16be202029dc0
ajinvarghesechandy1 <chandyajin@gmail.com>
2023-11-28 22:05:56 +0530

    added bg color to nav bar

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,20 +1,21 @@
 <nav>
-    <ul>
-      <li><strong>Compare</strong></li>
-    </ul>
-    <ul>
-      <li><a href="/compare-pdf">Pdf</a></li>
-      <li><a href="/compare-text">Text</a></li>
-      <li><a href="/compare-image">Image</a></li>
-    </ul>
-  </nav>
+	<ul>
+		<li><strong>Compare</strong></li>
+	</ul>
+	<ul>
+		<li><a href="/compare-pdf">Pdf</a></li>
+		<li><a href="/compare-text">Text</a></li>
+		<li><a href="/compare-image">Image</a></li>
+	</ul>
+</nav>
 
-  <style>
-    nav{
-        display: flex;
-        justify-content: space-around;
-        position: fixed;
-  width: 100%;
-  top: 0;
-    }
-  </style>
+<style>
+	nav {
+		display: flex;
+		justify-content: space-around;
+		position: fixed;
+		width: 100%;
+		top: 0;
+		background-color: rgba(240, 248, 255, 0.295);
+	}
+</style>
